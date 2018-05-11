c51d2ae90c403e417b088b8c66cbeca6454d9fb5
wjlee <wjlee@220.66.61.58>
2018-05-11 11:27:17 +0900

    의존성을 보여주기 위한 예제

---
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -0,0 +1,17 @@
+package polymorphism;
+
+public class LgTV {
+	
+	public void turnOn() {
+		System.out.println("Lg TV --- 전원켠다.");
+	}
+	public void turnOff() {
+		System.out.println("Lg TV --- 전원끈다.");
+	}
+	public void soundUp() {
+		System.out.println("Lg TV --- 소리 올린다.");
+	}
+	public void soundDown() {
+		System.out.println("Lg TV --- 소리 내린다.");
+	}
+}
---
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -0,0 +1,17 @@
+package polymorphism;
+
+public class SamsungTV {
+	
+	public void powerOn() {
+		System.out.println("Samsung TV --- 전원켠다.");
+	}
+	public void powerOff() {
+		System.out.println("Samsung TV --- 전원끈다.");
+	}
+	public void volumeUp() {
+		System.out.println("Samsung TV --- 소리 올린다.");
+	}
+	public void volumeDown() {
+		System.out.println("Samsung TV --- 소리 내린다.");
+	}
+}
