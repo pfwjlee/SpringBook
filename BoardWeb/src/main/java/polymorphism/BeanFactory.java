502810e22ea0e21266c81fe7ac3384c1366112ef
wjlee <wjlee@220.66.61.58>
2018-05-11 11:50:35 +0900

    BeanFactory 사용

---
+++ BoardWeb/src/main/java/polymorphism/BeanFactory.java
@@ -0,0 +1,12 @@
+package polymorphism;
+
+public class BeanFactory {
+	public Object getBeans(String beanName) {
+		if(beanName.equals("samsung")) {
+			return new SamsungTV();
+		} else if(beanName.equals("lg")) {
+			return new LgTV();
+		}
+		return null;
+	}
+}
--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -2,7 +2,9 @@ package polymorphism;
 
 public class TVUser {
 	public static void main(String[] args) {
-		TV tv = new SamsungTV();
+		BeanFactory factory = new BeanFactory();
+		
+		TV tv = (TV)factory.getBean(args[0]);
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
