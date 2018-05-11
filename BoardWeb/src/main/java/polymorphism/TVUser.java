100b5a010c8975417b3e29629adb538d0425bddd
wjlee <wjlee@220.66.61.58>
2018-05-11 11:37:54 +0900

    push

--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -2,10 +2,10 @@ package polymorphism;
 
 public class TVUser {
 	public static void main(String[] args) {
-		LgTV tv = new LgTV();
-		tv.turnOn();
-		tv.soundUp();
-		tv.soundDown();
-		tv.turnOff();
+		TV tv = new SamsungTV();
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
 	}
 }
